2617fcb6ce431e6a29d9afafa3a9df4a5a940bab
Zack Drescher <zackdrescher@Zacks-MacBook-Pro.local>
2018-08-29 19:26:49 -0400

    added timing to ingestion scripts

--- full_ing.py
+++ full_ing.py
@@ -1,7 +1,15 @@
 from ingestor import Ingestor
+from datetime import datetime
 
 ing = Ingestor()
 
 ing.clear_database()
 
+start = datetime.now()
+
 ing.ingest()
+
+end = datetime.now()
+t = end - start
+print("Ingestion ended at ", end)
+print("Ingestion ended in ", end)
--- s_ing.py
+++ s_ing.py
@@ -1,7 +1,15 @@
 from ingestor import Ingestor
+from datetime import datetime
 
 ing = Ingestor()
+
+start = datetime.now()
 
 ing.clear_database()
 
 ing.ingest(limit=20)
+
+end = datetime.now()
+t = end - start
+print("Ingestion ended at ", end)
+print("Ingestion ended in ", end)
